44447978d058e48e95e60c3377b2bd5531068199
basiliusic <basiliusic@gmail.com>
2021-10-31 19:27:15 +0300

    chore(project): add platform targets #ios-1

---
+++ Logger/Logger.h
@@ -0,0 +1,18 @@
+//
+//  Logger_Mac_OS.h
+//  Logger Mac OS
+//
+//  Created by basilic on 31.10.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Logger_Mac_OS.
+FOUNDATION_EXPORT double Logger_Mac_OSVersionNumber;
+
+//! Project version string for Logger_Mac_OS.
+FOUNDATION_EXPORT const unsigned char Logger_Mac_OSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logger_Mac_OS/PublicHeader.h>
+
+
